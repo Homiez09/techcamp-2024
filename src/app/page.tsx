29941b49b451f170d64230c159a00f985a868e91
Phumrapee Soenvanichakul <phumsoen123@gmail.com>
2024-05-01 08:55:28 +0700

    optimize web page for reduced internet usage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,17 +8,16 @@ import IconClient from "@/components/IconClient";
 export default function Page() {
   return (
     <>
-      <section id="index" className="min-h-screen bg-center bg-primary-texture bg-cover bg-no-repeat">
-        <div className="flex flex-col container h-screen items-center justify-center">
-          <Image src="/assets/logo/techcamp2-logowhite.png" priority alt="techcamp2-logo" className="md:m-[-120px] w-auto h-auto" width={600} height={600} />
-          <div className="flex flex-col items-center">
-            <ScrollToSection
+      <section id="index" className="relative min-h-screen">
+        <video src="/assets/background/loop-bg-small.mp4" autoPlay muted loop playsInline className="min-h-screen object-cover"/>
+        <div className="absolute inset-0 flex flex-col justify-center gap-5 items-center text-white bg-primary-texture">
+          <Image src="/assets/logo/techcamp2-logowhite.png" priority alt="techcamp2-logo" className="w-auto h-auto m-[-20px] md:m-[-120px]" width={600} height={600} />
+          <ScrollToSection
               className="bg-secondary rounded-xl px-8 py-3 flex flex-row items-center justify-center gap-2 font-bold text-white hover:cursor-pointer"
               to="about"
-              icon={<IconClient icon="maki:arrow" width="16" height="16" className="text-white"/>}
+              icon={<IconClient icon="maki:arrow" width="16" height="16" className="text-white" />}
               content='รายละเอียดค่าย' />
             <Image src="/assets/logo/tech-x-school.png" alt="techcamp2-logo" width={354} height={177} className="w-auto h-auto" />
-          </div>
         </div>
       </section>
 
