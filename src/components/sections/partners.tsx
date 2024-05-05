4505c67e759c2315da255f3856fced447d470930
Phumrapee Soenvanichakul <phumsoen123@gmail.com>
2024-05-05 19:52:09 +0700

    [+] Add partner logos to Partners section

--- src/components/sections/partners.tsx
+++ src/components/sections/partners.tsx
@@ -1,20 +1,30 @@
-import Section from "../section";
-import IconClient from "../IconClient";
+import Section from "../section";;
 import Image from "next/image";
 import Link from "next/link";
 
 export default function Partners() {
+    const partners = [
+        {
+            src: "ku-logo-high-res.png",
+            alt: "Kasetsart University",
+            href: "https://www.ku.ac.th/"
+        },
+        {
+            src: "SKR-logo-T.png",
+            alt: "Sakolrajwittayanukul School",
+            href: "#"
+        },
+    ];
     return (
         <Section id="partners" className="gap-8">
             <div className="container mx-auto">
                 <h2 className="text-3xl text-center font-bold mb-16">ร่วมสนับสนุนโดย</h2>
-                <div className="flex md:flex-row flex-wrap flex-col gap-20 justify-center items-center">
-                    <Link href="https://www.ku.ac.th/">
-                        <Image src="/assets/logo/ku-logo-high-res.png" alt="Kasetsart University Logo" width={150} height={150} />
-                    </Link>
-                    <Link href="#">
-                        <Image src="/assets/logo/ku-logo-high-res.png" alt="x Logo" width={150} height={150} />
-                    </Link>
+                <div className="flex md:flex-row md:flex-wrap flex-col gap-20 justify-center items-center">
+                    {partners.map((partner, index) => (
+                        <Link key={index} href={partner.href} className="relative h-[150px] w-[150px]">
+                            <Image loading="lazy" src={`/assets/logo/${partner.src}`} alt={partner.alt} fill className="object-contain w-full h-full" />
+                        </Link>
+                    ))}
                 </div>
             </div>
         </Section>
