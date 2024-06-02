d27c5a76548380f7004b33384bf3c1185dbfc51f
Phumrapee Soenvanichakul <phumsoen123@gmail.com>
2024-06-02 22:57:02 +0700

    add detail sponser

--- src/components/Sections/partners.tsx
+++ src/components/Sections/partners.tsx
@@ -17,17 +17,29 @@ export default function Partners() {
         {
             src: "sponsors/Kohkae.png",
             alt: "kohkae",
-            href: "#"
+            href: "#",
+            detail: <>
+                <small>บริษัท โรงงานแม่รวย จำกัด</small>
+                <small>เนลี่รสชีท จำนวน 300  ห่อ และโก๋แก่ถั่วลันเตาอบเกลือ จำนวน 200 ห่อ แลพโก๋แก่ถั่วลันเตาวาซาบิ จำนวน 200 ห่อ</small>
+            </>
         },
         {
             src: "sponsors/Lactasoy.png",
             alt: "Lactasoy",
-            href: "#"
+            href: "#",
+            detail: <>
+                <small>บริษัท เมเจอร์ ซีนีเพล็กซ์ กรุ๊ป จำกัด (มหาชน)</small>
+                <small>บัตรชมภาพยนต์ จำนวน 60 ใบ</small>
+            </>
         },
         {
             src: "sponsors/MajorCineplex.png",
             alt: "Major Cineplex",
-            href: "#"
+            href: "#",
+            detail: <>
+                <small>บริษัท แลคตาซอย จำกัด</small>
+                <small>ชุดสมุด-ปากกา 150 ชุด และ นม 15 ลัง</small>
+            </>
         },
     ];
     return (
@@ -36,14 +48,15 @@ export default function Partners() {
                 <h2 className="text-3xl text-center font-bold mb-16">ร่วมสนับสนุนโดย</h2>
                 <div className="flex md:flex-row md:flex-wrap flex-col gap-20 justify-center items-center">
                     {partners.map((partner, index) => (
-                        <Link key={index} href={partner.href} className="relative h-[150px] w-[150px]">
-                            <Image loading="lazy" src={`/assets/${partner.src}`} alt={partner.alt} sizes="2" fill className="object-contain" />
-                        </Link>
+                        <div className="flex flex-col w-[150px]">
+                            <Link key={index} href={partner.href} className="relative h-[150px] w-[150px]">
+                                <Image loading="lazy" src={`/assets/${partner.src}`} alt={partner.alt} sizes="2" fill className="object-contain" />
+                            </Link>
+                            {partner.detail}
+                        </div>
                     ))}
                 </div>
             </div>
         </Section>
-
-
     )
 }
